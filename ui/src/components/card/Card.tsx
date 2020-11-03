b5c1f3ea46661f3313144336a5deda3f91ac2dd8
Callum Williams <thegooball@gmail.com>
2020-11-03 16:19:10 +0000

    fix(Card): Fall back to instanceId if createAt is the same when ordering instances in stack

--- ui/src/components/card/Card.tsx
+++ ui/src/components/card/Card.tsx
@@ -7,23 +7,23 @@ import InstanceProps from "../../utils/InstanceProps";
 import { StateContext } from "../../context/StateContext";
 
 const CardBackground = styled.div`
-  background: ${props => props.theme.color.darkOne};
-  color: ${props => props.theme.color.lightOne};
+  background: ${(props) => props.theme.color.darkOne};
+  color: ${(props) => props.theme.color.lightOne};
   border-radius: 2px;
-  box-shadow: ${props => props.theme.shadow.card};
+  box-shadow: ${(props) => props.theme.shadow.card};
   width: 360px;
   display: flex;
   flex-direction: column;
 `;
 
 const instanceIds = (instances?: InstanceProps[]) => {
-  const ids = instances.map(instance => instance.instanceID);
+  const ids = instances.map((instance) => instance.instanceID);
   return ids[0] ? ids : undefined;
 };
 
 const getChosenOne = (instances?: InstanceProps[]) => {
   const chosenOne = instances.filter(
-    instance => instance.instanceIsChosenOne === true,
+    (instance) => instance.instanceIsChosenOne === true,
   );
   return chosenOne[0] || undefined;
 };
@@ -35,9 +35,12 @@ const Card = (props: { instances: InstanceProps[]; stackName: string }) => {
     <CardBackground>
       <CardHeader stackName={props.stackName} />
       {props.instances
-        .sort((a: InstanceProps, b: InstanceProps) =>
-          a.instanceCreatedAt < b.instanceCreatedAt ? -1 : 1,
-        )
+        .sort((a: InstanceProps, b: InstanceProps) => {
+          if (a.instanceCreatedAt === b.instanceCreatedAt) {
+            return a.instancePublicIP < b.instancePublicIP ? -1 : 1;
+          }
+          return a.instanceCreatedAt < b.instanceCreatedAt ? -1 : 1;
+        })
         .slice(0, instanceDisplayCount)
         .map((instance, i) => {
           return (
