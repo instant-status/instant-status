8233c65730449d27228508b6f7a04ddd89ecfd96
Callum Williams <thegooball@gmail.com>
2020-12-05 22:37:50 +0000

    feat(API): Add route for metadata

---
+++ api/src/controllers/v2/meta.ts
@@ -0,0 +1,20 @@
+import db from 'diskdb';
+import { InstanceProps } from '../../../../types/globalTypes';
+import response from '../../helpers/returnResponse';
+
+export const getMeta = (ctx: any) => {
+  const instances = db.instances.find() as InstanceProps[];
+  const stacks = new Set(instances.map((instance) => instance.stackName));
+  const versions = new Set(
+    instances.map((instance) => instance.instanceVersion)
+  );
+
+  const responseBody = {
+    activeVersions: [...versions],
+    instanceCount: instances.length,
+    stackCount: [...stacks].length,
+    maxInstanceCount: 3,
+  };
+
+  return response(ctx, 200, responseBody);
+};
