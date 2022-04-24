f8aca22370a270981d2cb4e896cc9af11f831f34
asahd <asahd@users.noreply.github.com>
2022-04-24 12:06:21 +0000

    feat: Add database seeders

---
+++ is-prisma/src/seed.ts
@@ -0,0 +1,27 @@
+import { prisma } from "./index";
+import { Users } from "@prisma/client";
+
+const userData: Omit<Users, "id" | "created_at">[] = [
+  {
+    email: "dev@instantstatus.local",
+    first_name: "IS",
+    last_name: "Dev",
+    is_super_admin: true,
+  },
+];
+
+const load = async () => {
+  try {
+    await prisma.users.createMany({
+      data: userData,
+    });
+    console.log("Seeded user data");
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+load();
